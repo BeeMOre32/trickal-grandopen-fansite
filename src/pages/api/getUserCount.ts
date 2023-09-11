cc06e65291e00d5cc2e232355e68354f9728d496
BeeMOre32 <windowssart01@gmail.com>
2023-09-11 14:30:16 +0900

    cheerio 로 스크랩핑이 막혀서 다시 작업

--- src/pages/api/getUserCount.ts
+++ src/pages/api/getUserCount.ts
@@ -1,25 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import axios from "axios";
-import cheerio from "cheerio";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Builder, By } from "selenium-webdriver";
+import chrome from "selenium-webdriver/chrome";
 
 type Data = {
-data: any
-}
+  data: string;
+};
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data>,
 ) {
+  let driver;
+
   try {
-    const response = await axios.get('https://trickcal.com/');
-    const html = response.data;
-    const $ = cheerio.load(html);
+    // 머리 없는 모드로 브라우저를 설정
+    const options = new chrome.Options();
+    options.headless();
 
-    const data = $('p.el.count').text();
+    driver = await new Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(options)
+      .build();
 
-    res.status(200).json({data});
+    await driver.get("https://trickcal.com/");
+
+    const element = await driver.findElement(By.css("p.el.count"));
+    const data = await element.getText();
+
+    res.status(200).json({ data });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ data: "An error occurred while scraping data" });
+  } finally {
+    // WebDriver 인스턴스 종료
+    if (driver) {
+      await driver.quit();
+    }
   }
 }
